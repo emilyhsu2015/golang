a5f33d2f1488aee1d0ad795cb03d662150a42f30
Emily <emily.hsu2015@gmail.com>
2018-06-06 17:02:19 +0800

    [新增]week1

---
+++ week/one.go
@@ -0,0 +1,33 @@
+// 宣告程式屬於哪個 package
+package main
+
+// 引入套件
+import (
+    "fmt"
+    "./workpackage"
+    "strconv"
+)
+
+// 程式執行入口
+func main() {
+    var a int = 1
+    var b int32 = 2
+    var c int64 = 3
+    //var d string = "999"
+    var e float32 = 88.8
+    var f float64 = 99.9
+    var x string = "I Love Golang_"
+
+
+    fmt.Println("a+b,用int型態印出 = ", a+int(b))
+    fmt.Println("a+b+c,用int型態印出 = ", a+int(b)+int(c))
+    fmt.Println("f/e,用float型態印出 = ", float32(f)/e)
+    fmt.Println("x+a,用string型態印出 = ", x+strconv.Itoa(a))
+
+    emilyTest("Emily");
+}
+
+func emilyTest(name string) {
+    fmt.Println("Hello ", name)
+    testpackage.TestPackage(name);
+}
---
+++ week/workPackage/onepackage.go
@@ -0,0 +1,11 @@
+// 宣告程式屬於哪個 package
+package testpackage
+
+// 引入套件
+import (
+    "fmt"
+)
+
+func TestPackage(name string) {
+    fmt.Println("Package ", name)
+}
